c0afdae2ea9ebd77d0c829ca54305e06bbe1ee14
Fabian Toro <helber.toro@gmail.com>
2019-10-22 16:05:53 -0500

    Initial state

---
+++ src/components/Helloworld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.sass';
+
+const Helloworld = () => (
+    <h1>Hello World</h1>
+);
+
+export default Helloworld;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                },
+            },
+            {
+                test: /\.(sass|css)$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader},
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { name: 'assets/[hash].[ext]' },
+                    }
+                ],
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        }),
+    ],
+};
