747483c29eb9ab7a5ab11a22872fb5eb51266199
sebastian07leal <sebastianleal2016@gmail.com>
2019-11-06 16:43:53 -0500

    Estilos al marker del mapa, y documentación en el código

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -4,18 +4,22 @@ import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 class MapContainer extends React.Component {
   
+
   constructor(props) {
     super(props);
 
+    /**No se le pueden dar estilos al mapa mediante clases, por lo tanto es necesario utilizar la propiedad style*/
     this.style = {
       width: '100%',
       height: '100%',
     };
+    /**La localizacion se cambiara posteriormente mediante propiedades*/
     this.location = {
       lat: 4.6560716,
       lng: -74.0595918,
     };
-    this.stylesDarck = [
+    /**Este array contiene las caracteristicas de estilos para un mapa nocturno se pueden tener varios estilos*/
+    this.stylesNight = [
       {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
       {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
       {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
@@ -95,10 +99,17 @@ class MapContainer extends React.Component {
         stylers: [{color: '#17263c'}]
       }
     ];
+    /**Estilos aplicados al Marjer de Google */
+    this.stylesMarker = {
+      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW, /**Forma del puntero*/
+      scale: 8, /**tamaño del Marker*/
+      strokeColor: '#FDD036', /**color del borde*/
+      strokeWeight: 3, /**grosor del borde*/
+      fillColor: '#FDD036', /**color de relleno*/
+      fillOpacity: 1, /**opacidad del relleno*/
+    };
 
   }
-
-
 
   render() {
     
@@ -107,18 +118,25 @@ class MapContainer extends React.Component {
         style={this.style}
         google={this.props.google}
         zoom={12}
-        initialCenter={this.location} 
-        styles = { this.stylesDarck }
+        /**Esta propiedad informa al mapa donde se debe centrar la vista*/
+        initialCenter={this.location}
+        /**Esta propiedad se encarga de dar atributos de estilos al mapa*/
+        styles = {this.stylesNight}
+        /**Este propiedad desactiva los botones por defecto del mapa*/
+        disableDefaultUI={true}
 
       >
         <Marker
           position={this.location}
+          icon={this.stylesMarker}
         />
       </Map>
     );
   }
 }
 
+/**Es neceario verificar el sporte dado por Google a "GoogleApiWrapper" dado que en futuras versiones puede que no tenga soporte*/
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  /**Es necesario agregar esta llave a las  variables de entorno de producción*/
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
