dd4f956946f993dd65c4534f9dc9a4cc177edd02
Fabian Toro <helber.toro@gmail.com>
2019-11-10 23:24:19 -0500

    Render Google map

---
+++ src/components/Map.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.onScriptLoad = this.onScriptLoad.bind(this)
+  }
+
+  onScriptLoad() {
+    const map = new window.google.maps.Map(
+      document.getElementById(this.props.id),
+      this.props.options);
+    this.props.onMapLoad(map)
+  }
+
+  componentDidMount() {
+    if (!window.google) {
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = `https://maps.google.com/maps/api/js?key=${process.env.GOOGLE_MAP_KEY}`;
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+      s.addEventListener('load', e => {
+        this.onScriptLoad()
+      })
+    } else {
+      this.onScriptLoad()
+    }
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} />
+    );
+  }
+}
+
+export default Map
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Map from '../components/Map';
 import '../assets/styles/Home.sass';
 import SeekerInput from '../components/SeekerInput';
 import UserIcon from '../components/UserIcon';
@@ -19,6 +19,22 @@ class Home extends React.Component {
   render() {
     return (
       <div className='container__home'>
+
+        <Map
+          id="map"
+          options={{
+            center: { lat: 4.605624, lng: -74.181895 },
+            zoom: 14
+          }}
+          onMapLoad={map => {
+            var marker = new window.google.maps.Marker({
+              position: { lat: 41.0082, lng: 28.9784 },
+              map: map,
+              title: 'Hello Istanbul!'
+            });
+          }}
+        />
+
         <div className='container__home--header'>
           <SeekerInput focus={this.autoFocus} direction={this.handleFocus} type='text' placeholder='¿A dónde quieres ir hoy?' />
           <UserIcon href='#' background='https://img.icons8.com/doodle/48/000000/user-female--v4.png' />
