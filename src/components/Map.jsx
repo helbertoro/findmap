4680b51101c6dea663a8a6102b524483368cec2a
Fabian Toro <helber.toro@gmail.com>
2019-11-11 23:37:17 -0500

    Change to use API in public

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,29 +3,13 @@ import React, { Component } from 'react';
 class Map extends Component {
   constructor(props) {
     super(props);
-    this.onScriptLoad = this.onScriptLoad.bind(this)
   }
 
-  onScriptLoad() {
+  componentDidMount() {
     const map = new window.google.maps.Map(
       document.getElementById(this.props.id),
       this.props.options);
     this.props.onMapLoad(map)
-  }
-
-  componentDidMount() {
-    if (!window.google) {
-      var s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.src = `https://maps.google.com/maps/api/js?key=${process.env.GOOGLE_MAP_KEY}&libraries=places&language=es-419`;
-      var x = document.getElementsByTagName('script')[0];
-      x.parentNode.insertBefore(s, x);
-      s.addEventListener('load', e => {
-        this.onScriptLoad()
-      })
-    } else {
-      this.onScriptLoad()
-    }
   }
 
   render() {
