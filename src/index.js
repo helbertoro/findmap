c77d55ffbe8a412778da2e6ee5c59b6c4be598da
Fabian Toro <helber.toro@gmail.com>
2019-11-14 11:58:09 -0500

    Redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 import App from './routes/App';
 
@@ -12,7 +12,8 @@ const initialState = {
     "Record": {}
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
     <Provider store={store}>
