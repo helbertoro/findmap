19f1b84a0a9121b91787b2453447068f1664f96e
Fabian Toro <helber.toro@gmail.com>
2019-11-26 14:51:45 -0500

    Change text

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -44,55 +44,54 @@ class Search extends React.Component {
   render() {
     return(
       <div className='container-search'>
-      <div className='container-search__header'>
-      <form onSubmit={this.handleSubmit} className="seeker">
-      <input id='pac-input' ref={this.elementRef} className='seeker__input' type='text' name='seeker' placeholder='¿A dónde quieres ir hoy?' />
-      <button type="submit" className='seeker__button'><img className="seeker__button--img" src="https://img.icons8.com/ios-filled/50/000000/search.png"/></button>
-      </form>
-      <SmallButtonDirection href={this.handleDirection} image='https://img.icons8.com/ios-glyphs/50/000000/return.png'/>
-      </div>
-      <section className='container-search__banner'>
-      <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/espresso-cup.png' />
-      <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/hamburger.png' />
-      <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/sushi.png' />
-      <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/beer.png' />
-      <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/cocktail.png' />
-      </section>
-      <section className='container-search__favorites'>
-      <TitleSearch text='Favoritos' />
-      <div className='container-search__favorites--favorite'>
-      <TextSearch href='#' text='Contray to popular belief' />
-      <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
-      </div>
-      <div className='container-search__favorites--favorite'>
-      <TextSearch href='#' text='Lorem Impsum is not simply random text' />
-      <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
-      </div>
-      <div className='container-search__favorites--favorite'>
-      <TextSearch href='#' text='Contray to popular belief' />
-      <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
-      </div>
-      <div className='container-search__favorites--favorite'>
-      <TextSearch href='#' text='Lorem Impsum is not simply random text' />
-      <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
-      </div>
-      <div className='container-search__favorites--button'>
-      <BigButton text='Ver todos los favoritos' />
-      </div>
-      </section>
-      {this.record.length > 0 &&
-        <section className='container-search__favorites'>
-        <TitleSearch text='Historial de búsqueda' />
-        <div className='container-search__favorites--history'>
-        {this.record}
+        <div className='container-search__header'>
+          <form onSubmit={this.handleSubmit} className="seeker">
+            <input id='pac-input' ref={this.elementRef} className='seeker__input' type='text' name='seeker' placeholder='¿A dónde quieres ir hoy?' />
+            <button type="submit" className='seeker__button'><img className="seeker__button--img" src="https://img.icons8.com/ios-filled/50/000000/search.png"/></button>
+          </form>
+          <SmallButtonDirection href={this.handleDirection} image='https://img.icons8.com/ios-glyphs/50/000000/return.png'/>
         </div>
+        <section className='container-search__banner'>
+          <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/espresso-cup.png' />
+          <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/hamburger.png' />
+          <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/sushi.png' />
+          <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/beer.png' />
+          <DirectionButton href='#' background='https://img.icons8.com/ios/50/000000/cocktail.png' />
         </section>
-      }
+        <section className='container-search__favorites'>
+          <TitleSearch text='Favoritos' />
+          <div className='container-search__favorites--favorite'>
+            <TextSearch href='#' text='Platzi' />
+            <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
+          </div>
+          <div className='container-search__favorites--favorite'>
+            <TextSearch href='#' text='Juan Valdez' />
+            <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
+          </div>
+          <div className='container-search__favorites--favorite'>
+            <TextSearch href='#' text='El Corral' />
+            <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
+          </div>
+          <div className='container-search__favorites--favorite'>
+            <TextSearch href='#' text='La Pizzeria' />
+            <DeleteButtonMin background='https://img.icons8.com/color/48/000000/minus.png' />
+          </div>
+          <div className='container-search__favorites--button'>
+            <BigButton text='Ver todos los favoritos' />
+          </div>
+        </section>
+        {this.record.length > 0 &&
+          <section className='container-search__favorites'>
+            <TitleSearch text='Historial de búsqueda' />
+            <div className='container-search__favorites--history'>
+              {this.record}
+            </div>
+          </section>
+        }
       </div>
-      );
-    }
-    
+    );
   }
+}
   
   
   const mapDispatchToProps = {
