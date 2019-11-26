716cd2ec374f16b07d7e6179c344e8cfe2b4348c
Fabian Toro <helber.toro@gmail.com>
2019-11-26 14:21:36 -0500

    redirect home

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -5,27 +5,35 @@ import UserFormButton from '../components/UserFormButton';
 import '../assets/styles/UserForm.sass'
 import Logo from '../assets/static/logo.png';
 
-const Login = () => (
-    <div className="container">
-        <div className="container__auth">
-            <img className="container__auth--logo" src={Logo} alt="FindMap"/>
-            <h2>Iniciar sesión</h2>
-            <form className="user-form" action="">
-                <UserFormInput type="email" placeholder="Correo electrónico"/>
-                <UserFormInput type="password" placeholder="Contraseña"/>
-                <UserFormButton value="Iniciar sesión"/>
-            </form>
-            <p>
-                <Link className="container__auth--link" to="/remember-password">
-                    Olvide mi contraseña
-                </Link>
-            </p>
-            <p>
-                <span>No tengo una cuenta</span> 
-                <Link className="container__auth--link ml-2" to="/">Registrarme</Link>
-            </p>            
+const Login = (props) => {
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        props.history.push('/home');
+    }
+
+    return (
+        <div className="container">
+            <div className="container__auth">
+                <img className="container__auth--logo" src={Logo} alt="FindMap"/>
+                <h2>Iniciar sesión</h2>
+                <form className="user-form" onSubmit={handleOnSubmit}>
+                    <UserFormInput type="email" placeholder="Correo electrónico"/>
+                    <UserFormInput type="password" placeholder="Contraseña"/>
+                    <UserFormButton value="Iniciar sesión"/>
+                </form>
+                <p>
+                    <Link className="container__auth--link" to="/remember-password">
+                        Olvide mi contraseña
+                    </Link>
+                </p>
+                <p>
+                    <span>No tengo una cuenta</span> 
+                    <Link className="container__auth--link ml-2" to="/">Registrarme</Link>
+                </p>            
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Login;
